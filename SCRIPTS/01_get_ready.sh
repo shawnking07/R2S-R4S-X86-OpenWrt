805341de96e27a007904451bedb7f325b3352a5d
Shawn.J <shawnking07@outlook.com>
2022-04-23 01:46:42 +0800

    v21.02.3

--- SCRIPTS/01_get_ready.sh
+++ SCRIPTS/01_get_ready.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 latest_release="$(curl -s https://github.com/openwrt/openwrt/tags | grep -Eo "v[0-9\.]+\-*r*c*[0-9]*.tar.gz" | sed -n '/[2-9][0-9]/p' | sed -n 1p | sed 's/.tar.gz//g')"
-git clone --single-branch -b ${latest_release} https://github.com/openwrt/openwrt openwrt_release
+git clone --single-branch -b v21.02.3 https://github.com/openwrt/openwrt openwrt_release
 git clone --single-branch -b openwrt-21.02 https://github.com/openwrt/openwrt openwrt
 rm -f ./openwrt/include/version.mk
 rm -f ./openwrt/include/kernel.mk
