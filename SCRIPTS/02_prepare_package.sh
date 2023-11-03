1b2224b8d260a9428e9518a95c8579dfdf94a44b
Shawn.J <shawnking07@outlook.com>
2023-11-03 23:30:43 +1100

    enable luci-app-daed

--- SCRIPTS/02_prepare_package.sh
+++ SCRIPTS/02_prepare_package.sh
@@ -70,7 +70,25 @@ git clone -b master --depth 1 https://github.com/brvphoenix/luci-app-wrtbwmon.gi
 git clone --depth 1 https://github.com/kiddin9/luci-app-xlnetacc.git package/lean/luci-app-xlnetacc
 # airsane
 git clone --depth 1 https://github.com/shawnking07/AirSane-openwrt.git package/new/airsane
+# daed
+git clone https://github.com/QiuSimons/luci-app-daed package/new/luci-app-dae
+echo '
+define KernelPackage/xdp-sockets-diag
+  SUBMENU:=$(NETWORK_SUPPORT_MENU)
+  TITLE:=PF_XDP sockets monitoring interface support for ss utility
+  KCONFIG:= \
+	CONFIG_XDP_SOCKETS=y \
+	CONFIG_XDP_SOCKETS_DIAG
+  FILES:=$(LINUX_DIR)/net/xdp/xsk_diag.ko
+  AUTOLOAD:=$(call AutoLoad,31,xsk_diag)
+endef
 
+define KernelPackage/xdp-sockets-diag/description
+ Support for PF_XDP sockets monitoring interface used by the ss tool
+endef
+
+$(eval $(call KernelPackage,xdp-sockets-diag))
+' >> package/kernel/linux/modules/netsupport.mk
 
 ### 最后的收尾工作 ###
 # Lets Fuck
