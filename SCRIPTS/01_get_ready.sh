37ba97151a14d46a0af4c2cbb74d4904970cb6be
Shawn.J <shawnking07@outlook.com>
2023-12-13 22:03:44 +1100

    update version

--- SCRIPTS/01_get_ready.sh
+++ SCRIPTS/01_get_ready.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-git clone --depth 1 -b v21.02.7 https://github.com/immortalwrt/immortalwrt openwrt
+git clone --depth 1 -b v23.05.1 https://github.com/immortalwrt/immortalwrt openwrt
 
 exit 0
